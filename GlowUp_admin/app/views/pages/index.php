de2f16e536ebfd78d38282d7f8dc0c5fdb495b1e
unknown <zaaferkenza08@gmail.com>
2023-01-19 16:05:17 +0100

    authentification & statistique

--- GlowUp_admin/app/views/pages/index.php
+++ GlowUp_admin/app/views/pages/index.php
@@ -11,7 +11,7 @@
   <div class="mb-6">
     <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your username</label>
     <input type="text" name="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-pink-500 focus:border-pink-500 block w-full p-2.5" placeholder="name@gmail.com" >
-    <span class="text-red-900"><?php echo $data['username_err'] ?></span>
+    <span class="text-red-600 font-bold"><?php echo $data['username_err'] ?></span>
   </div>
   <div class="mb-6">
     <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
