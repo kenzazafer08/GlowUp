cd4f9024445b672db1c9009dc7029ebb211ad736
unknown <zaaferkenza08@gmail.com>
2023-01-23 14:59:10 +0100

    finalisation du backend

--- GlowUp_admin/app/views/pages/dashbord.php
+++ GlowUp_admin/app/views/pages/dashbord.php
@@ -6,7 +6,7 @@
         <h5 class="mb-2 text-2xl font-semibold hover:text-pink-900">Products</h5>
         <i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
     <p class="mb-3 font-normal text-gray-500 dark:text-gray-400"><?php echo $data['nbp'] ?></p>
-    <a href="#" class="inline-flex items-center text-pink-600 hover:underline">
+    <a href="<?=URLROOT?>/dashboard/products" class="inline-flex items-center text-pink-600 hover:underline">
         See our Products
         <svg class="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
     </a>
@@ -17,7 +17,7 @@
         <h5 class="mb-2 text-2xl font-semibold hover:text-pink-900">Categories</h5>
     <i class="fa fa-bars" aria-hidden="true"></i></a>
     <p class="mb-3 font-normal text-gray-500 dark:text-gray-400"><?php echo $data['nbc'] ?></p>
-    <a href="#" class="inline-flex items-center text-pink-600 hover:underline">
+    <a href="<?=URLROOT?>/dashboard/categories" class="inline-flex items-center text-pink-600 hover:underline">
         See our Categories
         <svg class="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
     </a>
